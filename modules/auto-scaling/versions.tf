b4fa3142139d1e301c511026d00494e3e9f739b4
anmolnagpal <anmol@clouddrove.com>
2023-10-10 17:00:51 +0530

    feat: update version.tf and remove unuse variable

--- modules/auto-scaling/versions.tf
+++ modules/auto-scaling/versions.tf
@@ -1,15 +1,15 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.20.0"
+      version = ">= 5.18.1"
     }
     template = {
       source  = "hashicorp/template"
-      version = ">= 2.2.0"
+      version = "~>2.2.0"
     }
   }
 }
